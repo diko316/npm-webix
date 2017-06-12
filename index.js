6af81a16b7610b7d01c37d38362af706aac4f6e9
Diko Tech Slave <diko316@gmail.com>
2017-06-12 21:46:50 +0800

    1. fixed package.json rename of webix to webix-npm. 2. fixed entry script to support ES6 import default.

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 'use strict';
 
+var webix;
+
 require("./webix_debug.js");
 require("./webix.css");
 
 if (typeof global.webix === 'undefined') {
     throw new Error("Webix global variable do not exist.");
-    
 }
 
-module.exports = global.webix;
+webix = global.webix;
+webix["default"] = webix;
+
+module.exports = webix;
 
 
