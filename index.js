02336c1aedf24398e61059c489bb0b190dad9281
Diko Tech Slave <diko316@gmail.com>
2017-08-02 17:22:40 +0800

    totally removed webix.css!

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ var webix;
 
 require("./webix_debug.js");
 
-// must not be loaded by default
-//require("./webix.css");
-
 if (typeof global.webix === 'undefined') {
     throw new Error("Webix global variable do not exist.");
 }
